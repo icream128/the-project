06cc8f564c5be1b6aa3463ba542bad60861d13dc
Areerat <jznwalkerz@gmail.com>
2017-04-28 22:42:12 +0700

    ui design ver.1

--- app/src/main/java/com/example/icream/the_project/MainActivity.java
+++ app/src/main/java/com/example/icream/the_project/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.icream.the_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner subSpinner;
+    private Spinner input2;
+    private Spinner input3;
+
+    private ArrayList<String> subject = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        subSpinner = (Spinner)findViewById(R.id.input1);
+
+        createSubjectSpinner();
+
+    }
+    private void createSubjectSpinner(){
+        subject.add("Math");
+        subject.add("Thai");
+        subject.add("English");
+        subject.add("Art");
+        subject.add("Science");
+        subject.add("Computer");
     }
 }
